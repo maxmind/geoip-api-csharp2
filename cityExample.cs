11b4a49fd7400a9f7d61bb6b714841a40b6dedff
Thomas J Mather <geoipapi@maxmind.com>
2005-06-20 04:36:02 +0000

    if IP address not found, handle

--- cityExample.cs
+++ cityExample.cs
@@ -4,18 +4,30 @@ using System.IO;
 class Appa0{
   public static void Main(String[] args){
     //open the database
-    LookupService ls = new LookupService("/usr/local/share/GeoIP/GeoIPCity.dat", LookupService.GEOIP_STANDARD);
-    //get city location of the ip address
-    Location l = ls.getLocation("24.24.24.24");
-    Console.Write("country code " + l.countryCode + "\n");
-    Console.Write("country name " + l.countryName + "\n");
-    Console.Write("region " + l.region + "\n");
-    Console.Write("city " + l.city + "\n");
-    Console.Write("postal code " + l.postalCode + "\n");
-    Console.Write("latitude " + l.latitude + "\n");
-    Console.Write("longitude " + l.longitude + "\n");
-    Console.Write("dma code " + l.dma_code + "\n");
-    Console.Write("area code " + l.area_code + "\n");
+    try {
+      LookupService ls = new LookupService("/usr/local/share/GeoIP/GeoIPCity.dat", LookupService.GEOIP_STANDARD);
+      //get city location of the ip address
+      if (args.Length > 0) {
+        Location l = ls.getLocation(args[0]);
+        if (l != null) {
+          Console.Write("country code " + l.countryCode + "\n");
+          Console.Write("country name " + l.countryName + "\n");
+          Console.Write("region " + l.region + "\n");
+          Console.Write("city " + l.city + "\n");
+          Console.Write("postal code " + l.postalCode + "\n");
+          Console.Write("latitude " + l.latitude + "\n");
+          Console.Write("longitude " + l.longitude + "\n");
+          Console.Write("dma code " + l.dma_code + "\n");
+          Console.Write("area code " + l.area_code + "\n");
+        } else {
+          Console.Write("IP Address Not Found\n");
+        }
+      } else {
+        Console.Write("Usage: cityExample IPAddress\n");
+      }
+    } catch (System.Exception e) {
+      Console.Write("Error" + e.Message + "\n");
+    }
   }
 }
 
