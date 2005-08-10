5747e00b29a5f1ca6f1af74c2162b67818a4a66c
Thomas J Mather <geoipapi@maxmind.com>
2005-08-10 14:41:31 +0000

    Replaced Yugoslavia with Serbia and Montenegro

--- LookupService.cs
+++ LookupService.cs
@@ -71,7 +71,7 @@ public class LookupService{
         "SK","SL","SM","SN","SO","SR","ST","SV","SY","SZ","TC","TD","TF","TG",
         "TH","TJ","TK","TM","TN","TO","TP","TR","TT","TV","TW","TZ","UA","UG",
         "UM","US","UY","UZ","VA","VC","VE","VG","VI","VN","VU","WF","WS","YE",
-        "YT","YU","ZA","ZM","ZR","ZW","A1","A2","O1"};
+        "YT","CS","ZA","ZM","ZR","ZW","A1","A2","O1"};
 
     private static String[] countryName = {
             "N/A","Asia/Pacific Region","Europe","Andorra","United Arab Emirates",
@@ -109,7 +109,7 @@ public class LookupService{
             "Netherlands","Norway","Nepal","Nauru","Niue","New Zealand","Oman","Panama",
             "Peru","French Polynesia","Papua New Guinea","Philippines","Pakistan",
             "Poland","Saint Pierre and Miquelon","Pitcairn","Puerto Rico","" +
-            "Palestinian Territory","Portugal","Palau","Paraguay","Qatar",
+            "Palestinian Territory, Occupied","Portugal","Palau","Paraguay","Qatar",
             "Reunion","Romania","Russian Federation","Rwanda","Saudi Arabia",
             "Solomon Islands","Seychelles","Sudan","Sweden","Singapore","Saint Helena",
             "Slovenia","Svalbard and Jan Mayen","Slovakia","Sierra Leone","San Marino",
@@ -121,7 +121,7 @@ public class LookupService{
             "United States Minor Outlying Islands","United States","Uruguay","Uzbekistan",
             "Holy See (Vatican City State)","Saint Vincent and the Grenadines",
             "Venezuela","Virgin Islands, British","Virgin Islands, U.S.","Vietnam",
-            "Vanuatu","Wallis and Futuna","Samoa","Yemen","Mayotte","Yugoslavia",
+            "Vanuatu","Wallis and Futuna","Samoa","Yemen","Mayotte","Serbia and Montenegro",
             "South Africa","Zambia","Zaire","Zimbabwe","Anonymous Proxy",
             "Satellite Provider","Other"};
 
